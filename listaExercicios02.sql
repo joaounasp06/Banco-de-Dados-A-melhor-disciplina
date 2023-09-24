b31048d54fc4400de17144606e52a2d6f6868b9a
joaounasp06 <98972579+joaounasp06@users.noreply.github.com>
2023-09-23 22:07:46 -0300

    4

--- listaExercicios02.sql
+++ listaExercicios02.sql
@@ -1,7 +1,8 @@
-CREATE PROCEDURE sp_ContarLivrosPorCategoria(IN categoria_nome VARCHAR(100), OUT total INT)
+CREATE PROCEDURE sp_VerificarLivrosCategoria(IN categoria_nome VARCHAR(100), OUT possui_livros BOOLEAN)
 BEGIN
-    SELECT COUNT(*) INTO total
+    SELECT COUNT(*) INTO possui_livros
     FROM Livro
     INNER JOIN Categoria ON Livro.Categoria_ID = Categoria.Categoria_ID
     WHERE Categoria.Nome = categoria_nome;
+    SET possui_livros = (possui_livros > 0);
 END;
