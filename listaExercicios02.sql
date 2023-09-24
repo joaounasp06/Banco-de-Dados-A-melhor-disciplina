c7bb8047e33258744e8eaa33e20df22400b5641b
joaounasp06 <98972579+joaounasp06@users.noreply.github.com>
2023-09-23 22:22:33 -0300

    7
    7

--- listaExercicios02.sql
+++ listaExercicios02.sql
@@ -1,7 +1,12 @@
-CREATE PROCEDURE sp_TitulosPorCategoria(IN categoria_nome VARCHAR(100))
+CREATE PROCEDURE sp_AdicionarLivro(IN titulo_livro VARCHAR(255), IN editora_id INT, IN ano_publicacao INT, IN numero_paginas INT, IN categoria_id INT, OUT mensagem VARCHAR(255))
 BEGIN
-    SELECT Livro.Titulo
-    FROM Livro
-    INNER JOIN Categoria ON Livro.Categoria_ID = Categoria.Categoria_ID
-    WHERE Categoria.Nome = categoria_nome;
+    DECLARE EXIT HANDLER FOR 1062
+    BEGIN
+        SET mensagem = 'Erro: Já existe um livro com esse título.';
+    END;
+
+    INSERT INTO Livro (Titulo, Editora_ID, Ano_Publicacao, Numero_Paginas, Categoria_ID)
+    VALUES (titulo_livro, editora_id, ano_publicacao, numero_paginas, categoria_id);
+
+    SET mensagem = 'Livro adicionado com sucesso.';
 END;
