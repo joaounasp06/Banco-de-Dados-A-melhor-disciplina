2712f9ab0c42818f7be03de16ce74b939fcc64d0
joaounasp06 <98972579+joaounasp06@users.noreply.github.com>
2023-09-23 22:07:26 -0300

    3
    3

--- listaExercicios02.sql
+++ listaExercicios02.sql
@@ -1,6 +1,6 @@
-CREATE PROCEDURE sp_LivrosPorCategoria(IN categoria_nome VARCHAR(100))
+CREATE PROCEDURE sp_ContarLivrosPorCategoria(IN categoria_nome VARCHAR(100), OUT total INT)
 BEGIN
-    SELECT Livro.Titulo
+    SELECT COUNT(*) INTO total
     FROM Livro
     INNER JOIN Categoria ON Livro.Categoria_ID = Categoria.Categoria_ID
     WHERE Categoria.Nome = categoria_nome;
