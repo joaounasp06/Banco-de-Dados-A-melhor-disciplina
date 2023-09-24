e4b650cb47b9294ee6dde7df07cdfb6aae04d82f
joaounasp06 <98972579+joaounasp06@users.noreply.github.com>
2023-09-23 22:26:13 -0300

    8
    8

--- listaExercicios02.sql
+++ listaExercicios02.sql
@@ -1,12 +1,7 @@
-CREATE PROCEDURE sp_AdicionarLivro(IN titulo_livro VARCHAR(255), IN editora_id INT, IN ano_publicacao INT, IN numero_paginas INT, IN categoria_id INT, OUT mensagem VARCHAR(255))
+CREATE PROCEDURE sp_AutorMaisAntigo(OUT nome_autor VARCHAR(255))
 BEGIN
-    DECLARE EXIT HANDLER FOR 1062
-    BEGIN
-        SET mensagem = 'Erro: Já existe um livro com esse título.';
-    END;
-
-    INSERT INTO Livro (Titulo, Editora_ID, Ano_Publicacao, Numero_Paginas, Categoria_ID)
-    VALUES (titulo_livro, editora_id, ano_publicacao, numero_paginas, categoria_id);
-
-    SET mensagem = 'Livro adicionado com sucesso.';
+    SELECT CONCAT(Nome, ' ', Sobrenome) INTO nome_autor
+    FROM Autor
+    ORDER BY Data_Nascimento ASC
+    LIMIT 1;
 END;
